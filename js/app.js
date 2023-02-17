a496928054009f1aba11728cc178ae7b6feffd50
hnmahamud <hnmahamud@gmail.com>
2023-02-17 18:30:50 +0600

    another bug fix

--- js/app.js
+++ js/app.js
@@ -8,9 +8,9 @@ calculateBtn.addEventListener('click', function () {
     if(income && food && rent && clothes) {
         const totalExpenses = food + rent + clothes;
         const balance = income - totalExpenses;
-        if(balance >= totalExpenses) {
-            setTextFieldValue('total-expenses-field', totalExpenses);
-            setTextFieldValue('balance-field', balance);
+        if(income >= totalExpenses) {
+            setTextFieldValue('total-expenses-field', totalExpenses.toFixed(2));
+            setTextFieldValue('balance-field', balance.toFixed(2));
         }
         else {
             alert('Cut your coat according to your cloth!')
@@ -32,8 +32,8 @@ saveBtn.addEventListener('click', function() {
         const savings = income * (savingInput / 100);
         const remainingBalance = balance - savings;
     
-        setTextFieldValue('saving-amount-field', savings);
-        setTextFieldValue('remaining-balance-field', remainingBalance);
+        setTextFieldValue('saving-amount-field', savings.toFixed(2));
+        setTextFieldValue('remaining-balance-field', remainingBalance.toFixed(2));
     }
     else {
         alert('Please input valid number!');
